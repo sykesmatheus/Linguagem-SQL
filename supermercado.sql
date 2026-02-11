cd878f4af607f1482f1815c275d3f97d3d69ac97
matheus <reis70834@gmail.com>
2026-02-10 21:36:09 -0300

    supermercado

---
+++ supermercado.sql
@@ -0,0 +1,19 @@
+--create database supermercado;
+--CREATE TABLE cliente (
+--id int not null,
+--nome varchar(50) NOT NULL,
+--cpf varchar(11) NOT NULL,
+--constraint pk_id_cliente PRIMARY KEY (id)
+--)
+
+--create table produto(
+--id int not null,
+--descricao varchar(100) not null,
+--cod_barras varchar(14) not null,
+--valor decimal(16,2)
+--)
+
+--alter table produto add constraint pk_id_produto primary key (id)
+--alter table produto add column situacao boolean not null
+--alter table produto alter column descricao type varchar(200)
+--alter table produto drop column situacao
